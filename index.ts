2d902c01b5b23932d88930bca288a9cf097c3d65
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-04-16 21:06:17 -0600

    consumer done

--- index.ts
+++ index.ts
@@ -14,12 +14,6 @@ async function consumeData() {
       console.log("connection established with broker mqtt://52.206.90.192");
       // Suscribirse al topic "mqtt.metrics"
       // Se recibe el perfil del terrario
-      conn.subscribe("mqtt.metrics", (err) => {
-        if (err) {
-          throw new Error("Error subscribing to topic: " + err);
-        }
-        console.log("Subscribed to topic mqtt.metrics");
-      });
       conn.subscribe("mqtt/metrics/esp", (err) => {
         if (err) {
           throw new Error("Error subscribing to topic: " + err);
@@ -30,23 +24,7 @@ async function consumeData() {
 
     // Escuchar mensajes del topic "mqtt.metrics"
     conn.on("message", async (topic, message) => {
-      if (topic === "mqtt/metrics") {
-        const baseUrl = "";
-        const data = JSON.parse(message.toString());
-        const {
-          max_temp,
-          min_temp,
-          max_humidity,
-          min_humidity,
-          max_uv,
-          min_uv,
-        } = data.terrariumProfile;
-        const { id } = data;
-        const formatedURL = `${baseUrl}?max_temp=${max_temp}&min_temp=${min_temp}&max_humidity=${max_humidity}&min_humidity=${min_humidity}&max_uv=${max_uv}&min_uv=${min_uv}&id=${id}`;
-        console.log(data);
-        console.log(formatedURL)
-        //realizar peticion fetch
-      } else if (topic === "mqtt/metrics/esp") {
+      if (topic === "mqtt/metrics/esp") {
         //formatear información
         const data = JSON.parse(message.toString());
         console.log(data);
